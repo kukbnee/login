04cbb8cf5ba75d213610c778a15ca0019ea61fd4
kbim <gblim.citylabs@gmail.com>
2025-02-08 21:03:56 +0900

    init login by nodejs

---
+++ app.js
@@ -0,0 +1,32 @@
+// const http = require('http');
+// const app = http.createServer((req, res)=> {
+//   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8"});
+//   if (req.url === '/') {
+//     res.end('여기는 루트입니다.');
+//   } else if (req.url === '/login') {
+//     res.end('여기는 로그인화면입니다');
+//   }
+// });
+
+// app.listen(3001, ()=> {
+//   console.log('http로 기동된 서버입니다.');
+// });
+
+
+'use strict';
+
+// 모듈
+const express = require('express');
+const app = express();
+
+// 라우팅
+const home = require('./routes/home');
+
+// 앱 세팅
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+app.use('/', home);
+
+module.exports = app;
